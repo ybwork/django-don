33e55985a3602e729d1f4394d267478532a9533f
yetbetter <costilek@gmail.com>
2018-09-19 07:10:41 +0300

    V-8. Added AjaxResponseMixin

--- permits/urls.py
+++ permits/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from permits.views import CarView
 
 urlpatterns = [
-    path('cars', CarView.as_view(), name='car_create')
+    path('cars', CarView.as_view(), name='cars')
 ]
--- permits/views.py
+++ permits/views.py
@@ -1,11 +1,28 @@
+from django.http import JsonResponse, request
 from django.views.generic import CreateView
 
 from permits.models import Car
 
 
-class AjaxResponseMixin:
-    pass
+class AjaxResponseMixin(object):
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
 
-class CarView(CreateView):
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        return response
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk
+            }
+            return JsonResponse(data)
+        return response
+
+
+class CarView(AjaxResponseMixin, CreateView):
     model = Car
     fields = ['number', 'mark', 'model']
